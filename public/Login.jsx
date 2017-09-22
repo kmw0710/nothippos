b45dbabd71a40438eb659325acb56333f0a1a8e5
minwoo_kim <kminwoo0710@gmail.com>
2017-09-22 16:07:57 -0400

    done

--- public/Login.jsx
+++ public/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 export default class Login extends React.Component {
 	constructor(props) {
@@ -6,16 +7,33 @@ export default class Login extends React.Component {
 		this.state = {
 
 		}
-
-
 	}
+
 	render() {
+    var styleName = {
+    	fontSize: 40,
+    	color: "black",
+    	align: "center",
+    	"margin-left": "50px"
+    };
+    var dest = {
+    	"margin-left": "50px",
+    	"font-weight": "bold" 
+    };
+
     return (
   		<div>
-	        <h2> Not Hippos Hippos </h2>
-	        <h3> Plan your trip today! </h3>
-	        <button> <a href="/auth/facebook">Login with Facebook</a></button>
-  		</div>
+  			<div className="login" >
+  			<br /><br /><br /><br /><br /><br /><br /><br /><br />
+	  			<h1 style={dest}>Welcome to Destinations!</h1>
+		      <h2 style={{"margin-left": "50px"}}> Plan your trip today! </h2>
+		      <br /><br /><br />
+		        <Button style={dest}> <a href="/auth/facebook">Login with Facebook</a></Button>
+  			<br /><br /><br /><br /><br /><br /><br /><br /><br />
+  			<br /><br /><br /><br /><br /><br /><br /><br /><br />
+  			<h5 className="members" style={styleName}> Dillon, Eric, Max, Minwoo </h5>
+  			</div>
+  			</div>
     )
 	}
 }
